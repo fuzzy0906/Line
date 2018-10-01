621882f5ac295db0c7087851d7f25cfda7684a8c
fuzzy0906 <fuzzy0906@gmail.com>
2018-10-01 20:10:45 +0800

    test

---
+++ index.js
@@ -0,0 +1,34 @@
+var linebot = require('linebot');
+var express = require('express');
+var path = require('path');
+
+var bot = linebot({
+    channelId: '1611515190',
+    channelSecret: 'bb1f2897d150a316f528984af79f3992',
+    channelAccessToken: 'aXFDf7Z4w3ySEZyI0Lld4oy60NAiQxhx4IqTOkVsq2vsoxlBghRZhXAUuEmR6F/f6FS42ayJU1DSKGfs74JAofDSbXZL9/QC2v7S9tyLs32+MFNPL65D1SxJnplmFL/7BajpFAhOo95MlzT7p6CUrQdB04t89/1O/w1cDnyilFU='
+});
+
+var message = {
+    "你好":"我不好",
+    "你是誰":"我是ㄐ器人"
+};
+
+bot.on('message', function (event) {
+    var respone;
+    if(message[event.message.text]){
+        respone = message[event.message.text];
+    }else{
+        respone = '我不懂你說的 ['+event.message.text+']';
+    }
+	console.log(event.message.text + ' -> ' + respone);
+    bot.reply(event.replyToken, respone);
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
